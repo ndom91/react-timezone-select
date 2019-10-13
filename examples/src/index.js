450d35ae94bc64a072ec0ca772cda88e71e9231d
ndo@ndo3 <yo@ni.co.de>
2019-10-13 18:40:12 +0200

    add: gh-pages config

--- examples/src/index.js
+++ examples/src/index.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     }
   }
 
-  handleChange = (selectedOption) => {
+  handleChange = selectedOption => {
     this.setState({ selectedOption: selectedOption })
   }
 
