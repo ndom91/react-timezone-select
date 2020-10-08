4577a1b1a4d5799d838b8cb42bfea0de66737513
ndom91 <yo@ndo.dev>
2020-10-08 17:14:59 +0200

    update: example page w/ label style picker

--- examples/src/index.js
+++ examples/src/index.js
@@ -13,17 +13,31 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h2>react-timezone-select</h2>
-      <blockquote>Please make a selection</blockquote>
+      <div className='header'>
+        <h2>react-timezone-select</h2>
+        <p>
+          <a
+            href='https://ndo.dev'
+            alt='ndom91 homepage'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='author'
+          >
+            ndom91
+          </a>
+        </p>
+      </div>
       <div
         style={{
           display: 'flex',
-          width: '80%',
+          width: 'min(100%, 400px)',
           justifyContent: 'space-around',
+          marginTop: '50px',
         }}
         onChange={handleLabelChange}
         checked={labelStyle}
       >
+        <span>Label Style:</span>
         <label for='original'>
           <input
             type='radio'
@@ -66,7 +80,7 @@ const App = () => {
           onBlur={() => console.log('Blur!')}
         />
       </div>
-      <h3>Output:</h3>
+      <h3>Return Value:</h3>
       <div className='code'>
         <span style={{ fontWeight: '500' }}>{'{'}</span> <br />
         <span style={{ marginLeft: '20px', fontWeight: '500' }}>
