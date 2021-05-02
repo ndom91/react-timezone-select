76ba3f6164129063d2d1b178ca2e408b740a6152
ndom91 <yo@ndo.dev>
2021-05-02 21:33:14 +0200

    Updates

--- dist/App.js
+++
@@ -1,9 +0,0 @@
-import React from "../snowpack/pkg/react";
-import Timezone from "./Timezone.js";
-import "./App.css.proxy.js";
-function App({}) {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "App"
-  }, /* @__PURE__ */ React.createElement(Timezone, null));
-}
-export default App;
