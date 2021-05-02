55046c2473ed016d3a6d911634a054b6eabeccc1
ndom91 <yo@ndo.dev>
2021-05-02 19:55:16 +0200

    feat: move from webpack to snowpack devenv

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,95 +1,12 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import TimezoneSelect, {
-  i18nTimezones,
-  ITimezone,
-  ILabelStyle,
-} from '../../src'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-const App = () => {
-  const [selectedTimezone, setSelectedTimezone] = React.useState<ITimezone>({
-    value: '',
-    label: '',
-  })
-  const [labelStyle, setLabelStyle] = React.useState<ILabelStyle>('original')
+ReactDOM.render(<App />, document.getElementById('root'));
 
-  const handleLabelChange = event => {
-    setLabelStyle(event.target.value)
-  }
-
-  return (
-    <div className='App'>
-      <div className='header'>
-        <h2>react-timezone-select</h2>
-        <p>
-          <a
-            href='https://ndo.dev'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='author'
-          >
-            ndom91
-          </a>
-        </p>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          width: 'min(100%, 400px)',
-          justifyContent: 'space-around',
-          marginTop: '50px',
-        }}
-        onChange={handleLabelChange}
-      >
-        <span>Label Style:</span>
-        <label htmlFor='original'>
-          <input
-            type='radio'
-            id='original'
-            name='labelStyle'
-            value='original'
-            defaultChecked={labelStyle === 'original'}
-          />
-          original
-        </label>
-        <label htmlFor='altName'>
-          <input type='radio' id='altName' name='labelStyle' value='altName' />
-          altName
-        </label>
-        <label htmlFor='abbrev'>
-          <input type='radio' id='abbrev' name='labelStyle' value='abbrev' />
-          abbrev
-        </label>
-      </div>
-      <div className='select-wrapper'>
-        <TimezoneSelect
-          value={selectedTimezone}
-          onChange={setSelectedTimezone}
-          labelStyle={labelStyle}
-          onBlur={() => console.log('Blur!')}
-          timezones={{
-            ...i18nTimezones,
-            'America/Lima': 'Pittsburgh',
-            'Europe/Berlin': 'Frankfurt',
-          }}
-        />
-      </div>
-      <h3>Return Value:</h3>
-      <div className='code'>
-        <pre
-          style={{
-            margin: '0 20px',
-            fontWeight: 500,
-            fontFamily: 'monospace',
-          }}
-        >
-          {JSON.stringify(selectedTimezone, null, 2)}
-        </pre>
-      </div>
-    </div>
-  )
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
 }
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
--- src/index.tsx
+++ src/index.tsx
@@ -91,6 +91,11 @@ export const i18nTimezones: ICustomTimezone = {
 }
 
 export type ILabelStyle = 'original' | 'altName' | 'abbrev'
+export enum LabelType {
+  ORIGINAL = 'original',
+  ALTNAME = 'altName',
+  ABBREV = 'abbrev',
+}
 
 export type ITimezoneOption = {
   value: string
@@ -173,41 +178,66 @@ const TimezoneSelect = ({
   }
 
   const findFuzzyTz = (zone: string): ITimezoneOption => {
-    let currentTime;
+    let currentTime
     try {
-      currentTime = spacetime.now(zone);
+      currentTime = spacetime.now(zone)
+    } catch (err) {
+      return
     }
-    catch (err) {
-      return;
-    }
-    return getOptions.filter( (tz: ITimezoneOption) => (tz.offset === currentTime.timezone().current.offset))
-      .map( (tz: ITimezoneOption) => {
-        let score = 0;
-        if (currentTime.timezones[ tz.value.toLowerCase() ] && !!currentTime.timezones[ tz.value.toLowerCase() ].dst === currentTime.timezone().hasDst) {
-
-          if (tz.value.toLowerCase().indexOf(currentTime.tz.substr(currentTime.tz.indexOf('/') + 1)) !== -1) {
-            score += 8;
+    return getOptions
+      .filter(
+        (tz: ITimezoneOption) =>
+          tz.offset === currentTime.timezone().current.offset
+      )
+      .map((tz: ITimezoneOption) => {
+        let score = 0
+        if (
+          currentTime.timezones[tz.value.toLowerCase()] &&
+          !!currentTime.timezones[tz.value.toLowerCase()].dst ===
+            currentTime.timezone().hasDst
+        ) {
+          if (
+            tz.value
+              .toLowerCase()
+              .indexOf(
+                currentTime.tz.substr(currentTime.tz.indexOf('/') + 1)
+              ) !== -1
+          ) {
+            score += 8
           }
-          if (tz.label.toLowerCase().indexOf(currentTime.tz.substr(currentTime.tz.indexOf('/') + 1)) !== -1) {
-            score += 4;
+          if (
+            tz.label
+              .toLowerCase()
+              .indexOf(
+                currentTime.tz.substr(currentTime.tz.indexOf('/') + 1)
+              ) !== -1
+          ) {
+            score += 4
           }
-          if (tz.value.toLowerCase().indexOf(currentTime.tz.substr(0, currentTime.tz.indexOf('/')))) {
-            score += 2;
+          if (
+            tz.value
+              .toLowerCase()
+              .indexOf(currentTime.tz.substr(0, currentTime.tz.indexOf('/')))
+          ) {
+            score += 2
           }
-          score += 1;
+          score += 1
         } else if (tz.value === 'GMT') {
-          score += 1;
+          score += 1
         }
-        return { tz, score };
+        return { tz, score }
       })
-      .sort( (a, b) => b.score - a.score )
-      .map( ({ tz, score }) => tz )[0];
-  };
+      .sort((a, b) => b.score - a.score)
+      .map(({ tz, score }) => tz)[0]
+  }
 
   const parseTimezone = (zone: ITimezone) => {
     if (typeof zone === 'object' && zone.value && zone.label) return zone
     if (typeof zone === 'string') {
-      return getOptions.find(tz => tz.value === zone) || ( zone.indexOf('/') !== -1 && findFuzzyTz(zone) )
+      return (
+        getOptions.find(tz => tz.value === zone) ||
+        (zone.indexOf('/') !== -1 && findFuzzyTz(zone))
+      )
     } else if (zone.value && !zone.label) {
       return getOptions.find(tz => tz.value === zone.value)
     }
