28724523a0f5449c26a3bf51a0378eb04985e234
ndom91 <yo@ndo.dev>
2021-05-02 20:01:55 +0200

    Updates

---
+++ dist/App.js
@@ -0,0 +1,9 @@
+import React from "../_snowpack/pkg/react";
+import Timezone from "./Timezone.js";
+import "./App.css.proxy.js";
+function App({}) {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "App"
+  }, /* @__PURE__ */ React.createElement(Timezone, null));
+}
+export default App;
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
+import React from "../_snowpack/pkg/react";
+import ReactDOM from "../_snowpack/pkg/react-dom";
+import App from "./App.js";
+import "./index.css.proxy.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("root"));
+if (undefined /* [snowpack] import.meta.hot */ ) {
+  undefined /* [snowpack] import.meta.hot */ .accept();
+}
