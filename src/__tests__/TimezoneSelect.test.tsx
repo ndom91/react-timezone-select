9d5f0b966d2512f8019649dd033b0bd940ec9be9
ndom91 <yo@ndo.dev>
2021-08-13 19:26:33 +0200

    fix: jest import css

--- src/__tests__/TimezoneSelect.test.tsx
+++ src/__tests__/TimezoneSelect.test.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { render, findAllByText, fireEvent } from '@testing-library/react'
+import React from "react"
+import { render, findAllByText, fireEvent } from "@testing-library/react"
 
-import TimezoneSelect, { i18nTimezones } from '../index'
+import TimezoneSelect, { i18nTimezones } from "../index"
 
 // react-select react-testing-library jest example tests:
 // https://github.com/JedWatson/react-select/blob/master/packages/react-select/src/__tests__/Select.test.js
 
-test('snapshot - defaults', () => {
+test("snapshot - defaults", () => {
   const { container } = render(
-    <TimezoneSelect value={'Europe/Amsterdam'} onChange={e => e} />
+    <TimezoneSelect value={"Europe/Amsterdam"} onChange={e => e} />
   )
   expect(container).toMatchSnapshot()
 })
 
-test('loads and displays default timezone - passing string', async () => {
+test("loads and displays default timezone - passing string", async () => {
   const { getByText } = render(
-    <TimezoneSelect value={'Europe/Amsterdam'} onChange={e => e} />
+    <TimezoneSelect value={"Europe/Amsterdam"} onChange={e => e} />
   )
 
   expect(
@@ -25,12 +25,12 @@ test('loads and displays default timezone - passing string', async () => {
   ).toBeInTheDocument()
 })
 
-test('loads and displays default timezone - passing full object', async () => {
+test("loads and displays default timezone - passing full object", async () => {
   const { getByText } = render(
     <TimezoneSelect
       value={{
-        value: 'Europe/Amsterdam',
-        label: '(GMT+1:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna',
+        value: "Europe/Amsterdam",
+        label: "(GMT+1:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna",
       }}
       onChange={e => e}
     />
@@ -43,11 +43,11 @@ test('loads and displays default timezone - passing full object', async () => {
   ).toBeInTheDocument()
 })
 
-test('load and displays labelStyle - altName', async () => {
+test("load and displays labelStyle - altName", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={'America/Juneau'}
-      labelStyle='altName'
+      value={"America/Juneau"}
+      labelStyle="altName"
       onChange={e => e}
     />
   )
@@ -57,11 +57,11 @@ test('load and displays labelStyle - altName', async () => {
   ).toBeInTheDocument()
 })
 
-test('load and displays labelStyle - abbrev', async () => {
+test("load and displays labelStyle - abbrev", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={'America/Juneau'}
-      labelStyle='abbrev'
+      value={"America/Juneau"}
+      labelStyle="abbrev"
       onChange={e => e}
     />
   )
@@ -69,13 +69,13 @@ test('load and displays labelStyle - abbrev', async () => {
   expect(getByText(/\(GMT-[8-9]:00\) Alaska \(AH[D|S]T\)$/)).toBeInTheDocument()
 })
 
-test('load and displays custom timezone', async () => {
+test("load and displays custom timezone", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={'America/Lima'}
+      value={"America/Lima"}
       timezones={{
         ...i18nTimezones,
-        'America/Lima': 'Pittsburgh',
+        "America/Lima": "Pittsburgh",
       }}
       onChange={e => e}
     />
@@ -84,13 +84,13 @@ test('load and displays custom timezone', async () => {
   expect(getByText(/\(GMT-[5-6]:00\) Pittsburgh$/)).toBeInTheDocument()
 })
 
-test('load and displays only 2 custom timezone choices', async () => {
+test("load and displays only 2 custom timezone choices", async () => {
   const { debug, container } = render(
     <TimezoneSelect
-      value={''}
+      value={""}
       timezones={{
-        'America/Lima': 'Pittsburgh',
-        'Europe/Berlin': 'Frankfurt',
+        "America/Lima": "Pittsburgh",
+        "Europe/Berlin": "Frankfurt",
       }}
       menuIsOpen={true}
       onChange={e => e}
@@ -99,32 +99,29 @@ test('load and displays only 2 custom timezone choices', async () => {
   debug()
 
   const items = await findAllByText(container, /^\(GMT[+-][0-9]{1,2}:[0-9]{2}/)
-  console.log(items)
+  // console.log(items)
   expect(items).toHaveLength(2)
 })
 
-test('load and passes react-select props', async () => {
+test("load and passes react-select props", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={''}
+      value={""}
       timezones={{
         ...i18nTimezones,
-        'America/Lima': 'Pittsburgh',
+        "America/Lima": "Pittsburgh",
       }}
-      placeholder={'Please Select a Timezone'}
+      placeholder={"Please Select a Timezone"}
       onChange={e => e}
     />
   )
 
-  expect(getByText('Please Select a Timezone')).toBeInTheDocument()
+  expect(getByText("Please Select a Timezone")).toBeInTheDocument()
 })
 
-test('can determine timezone by approximate match', async () => {
+test("can determine timezone by approximate match", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value="Europe/Rome"
-      onChange={e => e}
-    />
+    <TimezoneSelect value="Europe/Rome" onChange={e => e} />
   )
 
   expect(
@@ -134,11 +131,11 @@ test('can determine timezone by approximate match', async () => {
   ).toBeInTheDocument()
 })
 
-test('select drop-downs must use the fireEvent.change', () => {
+test("select drop-downs must use the fireEvent.change", () => {
   const onChangeSpy = jest.fn()
   const { container } = render(
     <TimezoneSelect
-      value={'Europe/Amsterdam'}
+      value={"Europe/Amsterdam"}
       onChange={onChangeSpy}
       menuIsOpen={true}
     />
@@ -146,16 +143,16 @@ test('select drop-downs must use the fireEvent.change', () => {
 
   let selectOption = [
     ...container.querySelectorAll('div[id^="react-select"]'),
-  ].find(n => n.textContent === '(GMT-10:00) Hawaii')
+  ].find(n => n.textContent === "(GMT-10:00) Hawaii")
 
   fireEvent.click(selectOption)
 
   expect(onChangeSpy).toHaveBeenCalledTimes(1)
   expect(onChangeSpy).toHaveBeenCalledWith({
-    value: 'Pacific/Honolulu',
-    label: '(GMT-10:00) Hawaii',
-    altName: 'Pacific/Honolulu',
+    value: "Pacific/Honolulu",
+    label: "(GMT-10:00) Hawaii",
+    altName: "Pacific/Honolulu",
     offset: -10,
-    abbrev: 'Pacific/Honolulu',
+    abbrev: "Pacific/Honolulu",
   })
 })
