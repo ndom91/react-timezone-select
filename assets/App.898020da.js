b68902618702218f7694264fd819f0ee2b048b19
ndom91 <yo@ndo.dev>
2022-02-07 22:08:54 +0100

    Updates

---
+++ assets/App.898020da.js
@@ -0,0 +1,1 @@
+import"./vendor.bf51d9ce.js";import{j as e,T as p}from"./Timezone.fa3f4a62.js";function o({}){return e("div",{className:"App",children:e(p,{})})}export{o as A};
---
+++ assets/index.7f5c3f3a.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.bf51d9ce.js";import{A as o}from"./App.898020da.js";import{j as r}from"./Timezone.fa3f4a62.js";e.render(r(o,{}),document.getElementById("root"));
