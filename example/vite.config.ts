8fa5d3b6ea2898935ca5a729b2c7e3d8a0709c0a
ndom91 <yo@ndo.dev>
2022-02-07 22:08:43 +0100

    fix: example app

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-timezone-select',
+  base: '/react-timezone-select/',
+  build: {
+    rollupOptions: {
+      input: ['index.html', 'src/App.tsx', 'src/index.tsx', 'src/Timezone.tsx'],
+    },
+  },
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
   plugins: [react()],
 });
