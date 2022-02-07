f2e6182b036cd9087a84faec4c5d68e9621bbf51
ndom91 <yo@ndo.dev>
2022-02-07 21:13:43 +0100

    fix: example app

--- example/src/Timezone.tsx
+++ example/src/Timezone.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import spacetime from 'spacetime';
-import TimezoneSelect, { allTimezones } from '../../dist/esm';
-import type { ITimezone, ILabelStyle } from '../../dist/esm/dist';
+import TimezoneSelect, { allTimezones } from '../../dist/esm/index.js';
+import type { ITimezone, ILabelStyle } from '../../dist/esm/dist/index';
 
 const Timezone = () => {
   const [selectedTimezone, setSelectedTimezone] =
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,8 +5,6 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
