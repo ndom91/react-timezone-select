fbc0691d1804301dc7ee69ec6f5ce465cb8dceb3
ndom91 <yo@ndo.dev>
2023-03-27 10:31:30 +0200

    fix: restrict ReactSelect types

--- src/types/timezone.ts
+++ src/types/timezone.ts
@@ -1,10 +1,10 @@
-import type { Props as ReactSelectProps } from "react-select"
+import type { Props as ReactSelectProps } from 'react-select'
 
 export type ICustomTimezone = {
   [key: string]: string
 }
 
-export type ILabelStyle = "original" | "altName" | "abbrev"
+export type ILabelStyle = 'original' | 'altName' | 'abbrev'
 
 export interface ITimezoneOption {
   value: string
@@ -16,7 +16,8 @@ export interface ITimezoneOption {
 
 export type ITimezone = ITimezoneOption | string
 
-export interface Props extends Omit<ReactSelectProps, "onChange"> {
+export interface Props
+  extends Omit<ReactSelectProps<ITimezone, false>, 'onChange'> {
   value: ITimezone
   labelStyle?: ILabelStyle
   onChange?: (timezone: ITimezoneOption) => void
