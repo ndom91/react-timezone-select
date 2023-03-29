5fa68c9bea285f3a6265662c9055bf0a9b8b74a7
ndom91 <yo@ndo.dev>
2023-03-29 15:40:52 +0200

    Updates

---
+++ assets/App.ed54e24f.js
@@ -0,0 +1,1 @@
+import"./vendor.44f22f25.js";import{j as e,T as p}from"./Timezone.400a8901.js";function s({}){return e("div",{className:"App",children:e(p,{})})}export{s as A};
---
+++ assets/index.fa4dbd6c.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.44f22f25.js";import{A as o}from"./App.ed54e24f.js";import{j as r}from"./Timezone.400a8901.js";e.render(r(o,{}),document.getElementById("root"));
