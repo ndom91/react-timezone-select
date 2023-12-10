75cfc562393e35f560bd6c7537b53115f2c2b6c8
ndom91 <yo@ndo.dev>
2023-12-10 20:58:12 +0100

    Updates

--- assets/App.ff05c9f8.js
+++
@@ -1,1 +0,0 @@
-import{j as p,T as r}from"./Timezone.4699ad37.js";function e({}){return p("div",{className:"App",children:p(r,{})})}export{e as A};
--- assets/index.265948dd.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as r}from"./Timezone.4699ad37.js";import{A as o}from"./App.ff05c9f8.js";e.render(r(o,{}),document.getElementById("root"));
