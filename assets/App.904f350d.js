01b49d9b106c2b47e9576324ec152863781c48c4
ndom91 <yo@ndo.dev>
2024-01-21 01:26:11 +0100

    Updates

---
+++ assets/App.904f350d.js
@@ -0,0 +1,1 @@
+import{j as p,T as r}from"./Timezone.4ea64c46.js";function e(){return p("div",{className:"App",children:p(r,{})})}export{e as A};
--- assets/index.9eab00c4.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as r}from"./Timezone.54e57b16.js";import{A as o}from"./App.c6a2ae1b.js";e.render(r(o,{}),document.getElementById("root"));
