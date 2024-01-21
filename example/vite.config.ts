68a24a45cce027b0a97bfaa128ebe69c0750bec8
ndom91 <yo@ndo.dev>
2024-01-21 01:58:34 +0100

    fix: example app base path for gh pages deploy

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-timezone-select/',
   build: {
     rollupOptions: {
       input: ['index.html', 'src/App.tsx', 'src/index.tsx', 'src/Timezone.tsx'],
