386b79cd0eeb40602d58fbd0159f83f711a82b68
ndom91 <yo@ndo.dev>
2024-05-16 16:28:21 +0200

    fix: bump deps and allow react@19 in peerDeps

--- src/tests/TimezoneSelect.spec.tsx
+++ src/tests/TimezoneSelect.spec.tsx
@@ -1,29 +1,22 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-import { test, expect, vi } from 'vitest'
-import TimezoneSelect, { allTimezones } from '../../dist'
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import { test, expect, vi } from "vitest"
+import TimezoneSelect, { allTimezones } from "../../dist"
 
-// react-select react-testing-library jest example tests:
-// https://github.com/JedWatson/react-select/blob/master/packages/react-select/src/__tests__/Select.test.js
-
-test('loads and displays default timezone - passing string', async () => {
-  const { getByText } = render(
-    <TimezoneSelect value={'Europe/Amsterdam'} onChange={(e) => e} />,
-  )
+test("loads and displays default timezone - passing string", async () => {
+  const { getByText } = render(<TimezoneSelect value={"Europe/Amsterdam"} onChange={(e) => e} />)
 
   expect(
-    getByText(
-      /\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/,
-    ),
+    getByText(/\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/),
   ).toBeInTheDocument()
 })
 
-test('loads and displays default timezone - passing full object', async () => {
+test("loads and displays default timezone - passing full object", async () => {
   const { getByText } = render(
     <TimezoneSelect
       value={{
-        value: 'Europe/Amsterdam',
-        label: '(GMT+1:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna',
+        value: "Europe/Amsterdam",
+        label: "(GMT+1:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna",
         offset: 60,
       }}
       onChange={(e) => e}
@@ -31,19 +24,13 @@ test('loads and displays default timezone - passing full object', async () => {
   )
 
   expect(
-    getByText(
-      /\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/,
-    ),
+    getByText(/\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/),
   ).toBeInTheDocument()
 })
 
-test('load and displays labelStyle - altName', async () => {
+test("load and displays labelStyle - altName", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value={'America/Juneau'}
-      labelStyle="altName"
-      onChange={(e) => e}
-    />,
+    <TimezoneSelect value={"America/Juneau"} labelStyle="altName" onChange={(e) => e} />,
   )
 
   expect(
@@ -51,25 +38,21 @@ test('load and displays labelStyle - altName', async () => {
   ).toBeInTheDocument()
 })
 
-test('load and displays labelStyle - abbrev', async () => {
+test("load and displays labelStyle - abbrev", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value={'America/Juneau'}
-      labelStyle="abbrev"
-      onChange={(e) => e}
-    />,
+    <TimezoneSelect value={"America/Juneau"} labelStyle="abbrev" onChange={(e) => e} />,
   )
 
   expect(getByText(/\(GMT-[8-9]:00\) Alaska \(AK[D|S]T\)$/)).toBeInTheDocument()
 })
 
-test('load and displays custom timezone', async () => {
+test("load and displays custom timezone", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={'America/Lima'}
+      value={"America/Lima"}
       timezones={{
         ...allTimezones,
-        'America/Lima': 'Pittsburgh',
+        "America/Lima": "Pittsburgh",
       }}
       onChange={(e) => e}
     />,
@@ -78,68 +61,59 @@ test('load and displays custom timezone', async () => {
   expect(getByText(/\(GMT-[5-6]:00\) Pittsburgh$/)).toBeInTheDocument()
 })
 
-test('load and passes react-select props', async () => {
+test("load and passes react-select props", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={''}
+      value={""}
       timezones={{
         ...allTimezones,
-        'America/Lima': 'Pittsburgh',
+        "America/Lima": "Pittsburgh",
       }}
-      placeholder={'Please Select a Timezone'}
+      placeholder={"Please Select a Timezone"}
       onChange={(e) => e}
     />,
   )
 
-  expect(getByText('Please Select a Timezone')).toBeInTheDocument()
+  expect(getByText("Please Select a Timezone")).toBeInTheDocument()
 })
 
-test('can determine timezone by approximate match', async () => {
-  const { getByText } = render(
-    <TimezoneSelect value="Europe/Rome" onChange={(e) => e} />,
-  )
+test("can determine timezone by approximate match", async () => {
+  const { getByText } = render(<TimezoneSelect value="Europe/Rome" onChange={(e) => e} />)
 
   expect(
-    getByText(
-      /\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/,
-    ),
+    getByText(/\(GMT\+[1-2]:00\) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna$/),
   ).toBeInTheDocument()
 })
 
-// @TODO: Fix Rules of Hooks Error when using 'isMenuOpen' :thinking:
-test.skip('select drop-downs must use the fireEvent.change', () => {
+test("select drop-downs must use the fireEvent.change", () => {
   const onChangeSpy = vi.fn()
   const { container } = render(
-    <TimezoneSelect
-      value={'Europe/Amsterdam'}
-      onChange={onChangeSpy}
-      menuIsOpen={true}
-    />,
+    <TimezoneSelect value={"Europe/Amsterdam"} onChange={onChangeSpy} menuIsOpen={true} />,
   )
 
-  const selectOption = [
-    ...container.querySelectorAll('div[id^="react-select"]'),
-  ].find((n) => n.textContent === '(GMT-10:00) Hawaii')
+  const selectOption = [...container.querySelectorAll('div[id^="react-select"]')].find(
+    (n) => n.textContent === "(GMT-10:00) Hawaii",
+  )
 
   selectOption && fireEvent.click(selectOption)
 
   expect(onChangeSpy).toHaveBeenCalledTimes(1)
   expect(onChangeSpy).toHaveBeenCalledWith({
-    value: 'Pacific/Honolulu',
-    label: '(GMT-10:00) Hawaii',
-    altName: 'Hawaii-Aleutian Standard Time',
+    value: "Pacific/Honolulu",
+    label: "(GMT-10:00) Hawaii",
+    altName: "Hawaii-Aleutian Standard Time",
     offset: -10,
-    abbrev: 'HAST',
+    abbrev: "HAST",
   })
 })
 
-test('loads and does not throw on missing timezone', async () => {
+test("loads and does not throw on missing timezone", async () => {
   expect(() =>
     render(
       <TimezoneSelect
-        value={'Europe/Berlin'}
+        value={"Europe/Berlin"}
         timezones={{
-          'America/SmallTownMissing': 'Missing',
+          "America/SmallTownMissing": "Missing",
         }}
         onChange={(e) => e}
       />,
@@ -147,13 +121,13 @@ test('loads and does not throw on missing timezone', async () => {
   ).not.toThrowError(/Please enter an IANA timezone id/i)
 })
 
-test('load and does not omit timezone that isDST is true and doesn not have daylight definitions', async () => {
+test("load and does not omit timezone that isDST is true and doesn not have daylight definitions", async () => {
   const { getByText } = render(
     <TimezoneSelect
-      value={'Asia/Tehran'}
+      value={"Asia/Tehran"}
       timezones={{
         ...allTimezones,
-        'Asia/Tehran': 'Tehran',
+        "Asia/Tehran": "Tehran",
       }}
       onChange={(e) => e}
     />,
@@ -162,37 +136,25 @@ test('load and does not omit timezone that isDST is true and doesn not have dayl
   expect(getByText(/Tehran/)).toBeInTheDocument()
 })
 
-test('load and displays UTC', async () => {
+test("load and displays UTC", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value={'America/Juneau'}
-      displayValue="UTC"
-      onChange={(e) => e}
-    />,
+    <TimezoneSelect value={"America/Juneau"} displayValue="UTC" onChange={(e) => e} />,
   )
 
   expect(getByText(/\(UTC-[8-9]:00\) Alaska$/)).toBeInTheDocument()
 })
 
-test('can set current time (DST)', async () => {
+test("can set current time (DST)", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value={'America/Detroit'}
-      currentDatetime="2017-07-08"
-      onChange={(e) => e}
-    />,
+    <TimezoneSelect value={"America/Detroit"} currentDatetime="2017-07-08" onChange={(e) => e} />,
   )
 
   expect(getByText(/GMT-4/)).toBeInTheDocument()
 })
 
-test('can set current time (standard)', async () => {
+test("can set current time (standard)", async () => {
   const { getByText } = render(
-    <TimezoneSelect
-      value={'America/Detroit'}
-      currentDatetime="2017-01-27"
-      onChange={(e) => e}
-    />,
+    <TimezoneSelect value={"America/Detroit"} currentDatetime="2017-01-27" onChange={(e) => e} />,
   )
 
   expect(getByText(/GMT-5/)).toBeInTheDocument()
