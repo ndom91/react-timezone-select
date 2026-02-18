193e5f2ac3a0ea52bd5dc3fb5f8834b612c27342
ndom91 <yo@ndo.dev>
2026-02-18 15:56:51 +0100

    Bump all dependencies and migrate to Biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react"
+import viteTsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/tests/setupTests.ts",
   },
 })
