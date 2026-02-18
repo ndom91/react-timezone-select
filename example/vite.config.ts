3f8047dbc61327f070a2106b483ee845c69f200f
ndom91 <yo@ndo.dev>
2026-02-18 17:29:25 +0100

    Cleanup example app

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-timezone-select/',
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   build: {
     rollupOptions: {
       input: ['index.html', 'src/App.tsx', 'src/index.tsx', 'src/Timezone.tsx'],
